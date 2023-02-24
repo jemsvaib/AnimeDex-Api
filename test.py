e13057e8aede02b3dd6ef2575ce69dd1e9aec0ee
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2023-02-24 20:18:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import time
 from bs4 import BeautifulSoup as bs
 import pymongo
 import requests
-mUrl = 'mongodb+srv://TechZ:Bots@websitedata.wdycbvp.mongodb.net/?retryWrites=true&w=majority'
+
+mUrl = "mongodb+srv://TechZ:Bots@websitedata.wdycbvp.mongodb.net/?retryWrites=true&w=majority"
 
 db = pymongo.MongoClient(mUrl).AnimeDex
 viewsdb = db.views
@@ -15,51 +16,61 @@ not_working = []
 
 def top():
     global working, not_working
-    data = {}
-    ignore = ['home-animedex', 'search-animedex', 'home-blackanime']
-    x = viewsdb.find({}).sort([('views', -1), ('watch', -1)])
+    data = []
+    ignore = ["home-animedex", "search-animedex", "home-blackanime"]
+    x = viewsdb.find({})
+    d = []
     for i in x:
-        if i.get('anime') == 'one piece':
-            i['anime'] = 'one-piece'
+        d.append(
+            {"anime": i.get("anime"), "views": i.get("views"), "watch": i.get("watch")}
+        )
+    d = sorted(
+        d, key=lambda i: ((i.get("views") or 0) + (i.get("watch") or 0)), reverse=True
+    )
+
+    for i in d:
+        if i.get("anime") == "one piece":
+            i["anime"] = "one-piece"
         print(i, data, len(data))
-        if i.get('anime') in ignore:
+        if i.get("anime") in ignore:
             continue
         try:
-            if i.get('anime') in working:
-                data[i.get('anime')] = working[i.get('anime')]
-            elif i.get('anime') in not_working:
-                continue
-            else:
-                res = requests.get(
-                    'https://animedex.live/anime/' + i.get('anime'))
-                print('<a class="ep-btn" href="/episode/' in res.text,
-                      res.status_code)
-                if '<a class="ep-btn" href="/episode/' in res.text:
-                    soup = bs(res.text, 'html.parser')
-                    h1 = soup.find('div', 'details').find('h1')
-                    genre = soup.find('div', 'genre').find('a')
-                    ep = soup.find_all('span', 'item-des')[2]
-                    type = soup.find_all('span', 'item-des')[4]
-                    status = soup.find_all('span', 'item-des')[5]
-                    img = soup.find('div', 'poster').find('img')
-
-                    data[i.get('anime')] = [h1.text if h1 else 'None', genre.text if genre else 'None', ep.text if ep else 'None',
-                                            type.text if type else 'None', status.text if status else 'None', str(img.get('src'))]
-                    working[i.get('anime')] = data.get('anime')
+            res = requests.get("https://animedex.live/anime/" + i.get("anime"))
+            print('<a class="ep-btn" href="/episode/' in res.text, res.status_code)
+            if '<a class="ep-btn" href="/episode/' in res.text:
+                soup = bs(res.text, "html.parser")
+                h1 = soup.find("div", "details").find("h1")
+                genre = soup.find("div", "genre").find("a")
+                ep = soup.find_all("span", "item-des")[2]
+                type = soup.find_all("span", "item-des")[4]
+                status = soup.find_all("span", "item-des")[5]
+                img = soup.find("div", "poster").find("img")
+                data.append(
+                    (
+                        i.get("anime"),
+                        [
+                            h1.text if h1 else "None",
+                            genre.text if genre else "None",
+                            ep.text if ep else "None",
+                            type.text if type else "None",
+                            status.text if status else "None",
+                            str(img.get("src")),
+                        ],
+                    )
+                )
         except:
-            not_working.append(i.get('anime'))
             continue
 
         if len(data) == 10:
             break
-    print('Top Cache Updated...')
+    print("Top Cache Updated...")
 
-    topdb.update_one({'anime': 'top'}, {'$set': {'top': data}}, upsert=True)
+    topdb.update_one({"anime": "top"}, {"$set": {"top": data}}, upsert=True)
 
 
 while True:
     try:
         top()
-    except:
-        pass
-    time.sleep(10 * 60)
+    except Exception as e:
+        print(e)
+    time.sleep(5 * 60)
